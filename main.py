4c8369f144c3947b85aa77c007f9e4382d28ef37
thedatadonkeyy <127076060+thedatadonkeyy@users.noreply.github.com>
2023-03-19 16:36:25 +0000

    variables

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ print("Hello " + input("what is your name?")+ " !")
 
 #This code prints the number of characters in a user's name.
 print( len( input("What is your name? ") ) )
+
+#variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+a = input("a: ")
+b = input("b: ")
+
+#Changing variables 
+c=a
+a=b
+b=c
+
+num_hours ="5"
+print("There are "+num_hours+ " hours until midnight")
