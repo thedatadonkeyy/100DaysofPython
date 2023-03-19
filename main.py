82f0bb4e292ea32df3bb33f87b96fbbddca51a53
thedatadonkeyy <>
2023-03-19 02:19:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+print("Hello World!")
+print("Hello World!\nHello World!")
+print("Hello" + " " + "World!")
+
+input("What is your name?")
+
+print("Hello " + input("what is your name?")+ " !")
