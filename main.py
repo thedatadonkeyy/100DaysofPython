824a05fc9a238789489911ff18293b4ee1ec604b
thedatadonkeyy <127076060+thedatadonkeyy@users.noreply.github.com>
2023-03-19 02:58:50 +0000

    Code Print number of Characters

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ print("Hello" + " " + "World!")
 input("What is your name?")
 
 print("Hello " + input("what is your name?")+ " !")
+
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
