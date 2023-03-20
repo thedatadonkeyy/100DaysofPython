1438fd34021ca0f0095f57ca32ab92eb64180a7b
thedatadonkeyy <127076060+thedatadonkeyy@users.noreply.github.com>
2023-03-20 02:10:09 +0000

    BMI Calculator and life remaining.

--- Day2of100.py
+++ Day2of100.py
@@ -17,8 +17,24 @@ weight = input("enter your weight in kg: ")
 
 weight_int = int(weight)
 height_float = float(height)
-#Mathematical Formula to calculate
+#Mathematical Formula to calculate BMI
 BMI=(weight_int/ height_float**2)
 print(BMI)
 BMI_INT = int(BMI)
 print(BMI_INT)
+
+#rounding numbers
+print(round(8/3))
+
+# Calculate your life remaining in days, weeks, & months if you were to live 90 years
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+time_left=(90-int(age))
+months = time_left*12
+weeks = time_left*52
+days = time_left*365
+message = (f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+print(message)
